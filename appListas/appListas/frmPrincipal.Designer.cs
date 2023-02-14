0f789e78e7bb7dabf90c6bd2b1310a53f7ceff9e
Guillermo Pérez <112012124+Decimusk@users.noreply.github.com>
2023-02-14 08:28:33 -0600

    Add files via upload

---
+++ appListas/appListas/frmPrincipal.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace appListas
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvListado = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.grbMantenimiento = new System.Windows.Forms.GroupBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).BeginInit();
+            this.grbMantenimiento.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(27, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LISTADO DE PRODUCTOS";
+            // 
+            // dgvListado
+            // 
+            this.dgvListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListado.Location = new System.Drawing.Point(12, 63);
+            this.dgvListado.Name = "dgvListado";
+            this.dgvListado.Size = new System.Drawing.Size(343, 168);
+            this.dgvListado.TabIndex = 1;
+            this.dgvListado.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvListado_CellContentClick);
+            this.dgvListado.DoubleClick += new System.EventHandler(this.dgvListado_DoubleClick);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(54, 237);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(212, 43);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // grbMantenimiento
+            // 
+            this.grbMantenimiento.Controls.Add(this.btnGuardar);
+            this.grbMantenimiento.Controls.Add(this.txtStock);
+            this.grbMantenimiento.Controls.Add(this.txtPrecio);
+            this.grbMantenimiento.Controls.Add(this.txtMarca);
+            this.grbMantenimiento.Controls.Add(this.txtDescripcion);
+            this.grbMantenimiento.Controls.Add(this.txtNombre);
+            this.grbMantenimiento.Controls.Add(this.label6);
+            this.grbMantenimiento.Controls.Add(this.label5);
+            this.grbMantenimiento.Controls.Add(this.label4);
+            this.grbMantenimiento.Controls.Add(this.label3);
+            this.grbMantenimiento.Controls.Add(this.label2);
+            this.grbMantenimiento.Location = new System.Drawing.Point(375, 59);
+            this.grbMantenimiento.Name = "grbMantenimiento";
+            this.grbMantenimiento.Size = new System.Drawing.Size(319, 276);
+            this.grbMantenimiento.TabIndex = 3;
+            this.grbMantenimiento.TabStop = false;
+            this.grbMantenimiento.Text = "Mantenimiento de Productos";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(29, 224);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(269, 40);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(108, 187);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(167, 20);
+            this.txtStock.TabIndex = 1;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(108, 153);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(167, 20);
+            this.txtPrecio.TabIndex = 1;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(108, 119);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(167, 20);
+            this.txtMarca.TabIndex = 1;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(108, 85);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(167, 20);
+            this.txtDescripcion.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(108, 51);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(167, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(17, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 34);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Stock:";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(17, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 34);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Precio:";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(17, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 34);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Marca:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(17, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 34);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Descrpción:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(17, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 34);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre:";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 395);
+            this.Controls.Add(this.grbMantenimiento);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.dgvListado);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPrincipal";
+            this.Text = "FORMULARIO PRODUCTOS";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).EndInit();
+            this.grbMantenimiento.ResumeLayout(false);
+            this.grbMantenimiento.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvListado;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.GroupBox grbMantenimiento;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ appListas/appListas/frmPrincipal.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appListas
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private List<Producto> Productos = new List<Producto>();
+        private int edit_indice = -1;
+
+        private void actualizarGrid()
+        {
+            dgvListado.DataSource = null;
+            dgvListado.DataSource = Productos;
+        }
+
+        private void reseteo()
+        {
+            txtNombre.Clear();
+            txtDescripcion.Clear();
+            txtMarca.Clear();
+            txtPrecio.Clear();
+            txtStock.Clear();
+        }
+
+        private void dgvListado_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgvListado_DoubleClick(object sender, EventArgs e)
+        {
+            DataGridViewRow selected = dgvListado.SelectedRows[0];
+            int posicion = dgvListado.Rows.IndexOf(selected);
+            edit_indice = posicion;
+
+            Producto product = Productos[posicion];
+
+            txtNombre.Text = product.Nombre;
+            txtDescripcion.Text = product.Descripcion;
+            txtMarca.Text = product.Marca;
+            txtPrecio.Text = Convert.ToString(product.Precio);
+            txtStock.Text = Convert.ToString(product.Stock);
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Producto product = new Producto();
+            product.Nombre = txtNombre.Text;
+            product.Descripcion = txtDescripcion.Text;
+            product.Marca = txtMarca.Text;
+            product.Precio = float.Parse(txtPrecio.Text);
+            product.Stock = int.Parse(txtStock.Text);
+
+            if (edit_indice > -1)
+            {
+                Productos[edit_indice] = product;
+                edit_indice = -1;
+            }
+            else
+            {
+                Productos.Add(product);
+            }
+
+            actualizarGrid();
+            reseteo();
+            
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (edit_indice > -1)
+            {
+                Productos.RemoveAt(edit_indice);
+                edit_indice = -1;
+                reseteo();
+                actualizarGrid();
+            }
+            else
+            {
+                MessageBox.Show("Dar doble Clic sobre algun elemento");
+            }
+        }
+    }
+}
